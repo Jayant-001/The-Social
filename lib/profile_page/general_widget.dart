21d81948bc086c8e78801672de74ba2078f4afc1
palprashant2002 <palprashant234@gmail.com>
2022-01-23 00:23:28 +0530

    add see more option

--- lib/profile_page/general_widget.dart
+++ lib/profile_page/general_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:the_social/chat/constants.dart';
 
 Widget showfpf({required int num, required String text}) {
   return Column(
@@ -64,38 +65,6 @@ Widget rowview({required String text, required var ic}) {
 
 //___________________________Card_View____________________________________________________
 
-Widget CardView({required String head, required List data}) {
-  return Card(
-    elevation: 5,
-    shadowColor: Colors.black,
-    color: Colors.white,
-    child: Column(
-      children: [
-        Container(
-          height: 24,
-          alignment: Alignment.topLeft,
-          child: tilehead(text: head),
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        Container(
-          height: 40 * data.length.toDouble(),
-          child: ListView.separated(
-            padding: EdgeInsets.all(0),
-            separatorBuilder: (context, index) =>
-                Divider(height: 0, color: Colors.white),
-            itemCount: data.length,
-            itemBuilder: (BuildContext context, int index) {
-              return tileview(text: data[index], ic: Icons.ac_unit_outlined);
-            },
-          ),
-        )
-      ],
-    ),
-  );
-}
-
 Widget Icontileview({required String txt, required var img}) {
   return ListTile(
       title: Text(txt),
@@ -109,14 +78,6 @@ Widget Icontileview({required String txt, required var img}) {
         )),
       ));
 }
-
-
-
-
-
-
-
-
 
 // _______________Functions to make bottomsheeta_______________________________________
 
@@ -173,3 +134,83 @@ Widget Icontileview({required String txt, required var img}) {
 
 //___________________________Function for editing image_________________________
 
+class Cardview extends StatefulWidget {
+  Cardview(
+      {Key? key,
+      required this.head,
+      required this.data,
+      this.ismore = "See More",
+      this.icontype = Icons.arrow_drop_down_outlined})
+      : super(key: key);
+  String head;
+  List data;
+  String ismore;
+  IconData icontype;
+  @override
+  _CardviewState createState() => _CardviewState();
+}
+
+List<Widget> fetchtiles(List<Widget> lst, int l, List dat) {
+  for (int i = 1; i <= l; i++) {
+    lst.add(tileview(text: dat[i - 1], ic: Icons.ac_unit_outlined));
+  }
+  return lst;
+}
+
+class _CardviewState extends State<Cardview> {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> tiles = [tilehead(text: widget.head)];
+    int len = (widget.ismore != "See More"
+        ? widget.data.length
+        : (widget.data.length > 2 ? 2 : widget.data.length));
+    tiles = fetchtiles(tiles, len, widget.data);
+    tiles.add(Container(
+      color: chatPrimaryColor,
+      height: 30,
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.ismore,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Icon(
+              widget.icontype,
+              size: 25,
+              color: Colors.white,
+            ),
+          ],
+        ),
+        onTap: () => setState(() {
+          if (widget.ismore == "See More") {
+            widget.ismore = "See Less";
+            widget.icontype = Icons.arrow_drop_up_outlined;
+          } else {
+            widget.ismore = "See More";
+            widget.icontype = Icons.arrow_drop_down_outlined;
+          }
+        }),
+      ),
+    ));
+    return Card(
+      elevation: 5,
+      shadowColor: Colors.black,
+      color: Colors.white,
+      child: Container(
+        alignment: Alignment.topLeft,
+        child: Column(
+          children: tiles,
+        ),
+      ),
+    );
+  }
+}
